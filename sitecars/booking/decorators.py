f1bb102b2e44512a62f5b7efaf401ed23630e9d8
Tolik pwmz <pal3noa@ya.ru>
2024-08-20 21:09:55 +0300

    тестовый комит3

--- sitecars/booking/decorators.py
+++ sitecars/booking/decorators.py
@@ -18,6 +18,7 @@ def booking_decorator(func):
         trip = Publishing_a_trip.objects.get(slug=trip_slug)
         if trip.reserved_seats < trip.free_seating:
             trip.reserved_seats += 1
+            
             trip.save()
             return func(trip_slug, request, trip)
         else:
