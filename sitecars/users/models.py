944826277ef4063e0253df8687a1c878fda94770
Tolik pwmz <pal3noa@ya.ru>
2024-11-29 22:03:53 +0300

    добавил значение по умолчанию в поле фото модели юзер

--- sitecars/users/models.py
+++ sitecars/users/models.py
@@ -8,8 +8,8 @@ from bestcar.models import Category
 class User(AbstractUser):
     photo = models.ImageField(
         upload_to='users/$Y/%m/%d/',
-        blank=True, null=True,
-        verbose_name='Фотография')
+        verbose_name='Фотография',
+        default='users/default_user_image.jpg')
     category = models.ForeignKey(
         Category,
         on_delete=models.PROTECT,
