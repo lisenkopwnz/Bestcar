07e7c38838b6c805b73e0fd58d89453eb5b57aad
Tolik pwmz <pal3noa@ya.ru>
2024-12-03 22:35:37 +0300

    изменил html и css входа в аккаунт а также изменил форму авторищации

--- sitecars/users/forms.py
+++ sitecars/users/forms.py
@@ -1,27 +1,46 @@
 from PIL import Image
 from django import forms
-from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, BaseUserCreationForm
+from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth.forms import (
+                                    AuthenticationForm,
+                                    PasswordChangeForm,
+                                    BaseUserCreationForm
+                                    )
 from django.forms import ModelForm
 from bestcar.models.category import Category
+from users.services.services_images import (
+                                        load_default_image,
+                                        convert_to_jpeg_if_needed,
+                                        resize_image
+                                        )
 
-import logging
-
-from users.services.services_images import load_default_image, convert_to_jpeg_if_needed, resize_image
-
-logger = logging.getLogger('duration_request_view')
 
 class LoginUserForms(AuthenticationForm):
     username = forms.CharField(label='Логин',
-                               widget=forms.TextInput(attrs={'class': 'form-input'}))
+                               widget=forms.TextInput(attrs={'class': 'form-input', 'id': 'username-input'})
+                               )
     password = forms.CharField(label='Пароль',
-                               widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-
-
+                               widget=forms.PasswordInput(attrs={'class': 'form-input','id': 'password1-input'})
+                               )
 
     class Meta:
         model = get_user_model()
         fields = ['username', 'password']
+
+    def clean(self):
+        username = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
+
+        if username is not None and password:
+            self.user_cache = authenticate(
+                self.request, username=username, password=password
+            )
+            if self.user_cache is None:
+                raise self.get_invalid_login_error()
+            else:
+                self.confirm_login_allowed(self.user_cache)
+
+        return self.cleaned_data
 
 
 class Registration_User_Form(BaseUserCreationForm):
